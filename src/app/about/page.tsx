d29fbe2e2361186c967f4198de1514fd9a5c76b4
Vinicius Gabriel <viiniciusgs03@gmail.com>
2023-03-06 21:09:39 -0300

    :sparkles: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <section className="mt-40 pb-40">
+      <p>
+        Hey there, welcome to my personal website! I&apos;m [Your Name] and
+        I&apos;m stoked to share some stuff about me with you.
+      </p>
+      <p className="mt-8">
+        I&apos;m a [Your Profession/Field of Interest] and I&apos;m all about
+        [Your Passion/Interest]. I&apos;ve been doing this gig for [Number of
+        Years] years now and I&apos;ve picked up a ton of knowledge and
+        experience along the way.
+      </p>
+      <p className="mt-8">
+        When I&apos;m not at work, you can catch me [Your Hobbies/Interests] or
+        [Another Hobby/Interest]. Plus, I&apos;m always down for an adventure
+        and exploring new places whenever the opportunity arises.
+      </p>
+      <p className="mt-8">
+        This website is my way of sharing my [Your Field of Expertise] expertise
+        and connecting with like-minded folks. You can expect to find [What
+        Visitors Can Expect on Your Website]
+      </p>
+      <p className="mt-8">
+        Thanks for stopping by and I hope you dig getting to know me and my
+        work.
+      </p>
+    </section>
+  )
+}
