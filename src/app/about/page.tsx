98f1c47cf74b59e0dbf3151ba002f1626e46d4a6
Vinicius Gabriel <viiniciusgs03@gmail.com>
2023-03-07 22:10:29 -0300

    :sparkles: add original text in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,28 +6,21 @@ export default function About() {
   return (
     <section className="mt-40 pb-40 md:mt-20 md:pb-20">
       <p>
-        Hey there, welcome to my personal website! I&apos;m [Your Name] and
-        I&apos;m stoked to share some stuff about me with you.
+        Hey there! I&apos;m Vinicius Gabriel, I work as a Software Engineer and
+        I&apos;m passionate about creating attractive and interactive web
+        experiences.
       </p>
       <p className="mt-8">
-        I&apos;m a [Your Profession/Field of Interest] and I&apos;m all about
-        [Your Passion/Interest]. I&apos;ve been doing this gig for [Number of
-        Years] years now and I&apos;ve picked up a ton of knowledge and
-        experience along the way.
+        I was born in Sao Paulo, Brazil, I&apos;m 20 years old and I&apos;m
+        currently studying System Analysis and Development at the FIAP. I got to
+        know the world of programming in 2016 and since then I&apos;ve been
+        getting deeper and deeper into the subject.
       </p>
       <p className="mt-8">
-        When I&apos;m not at work, you can catch me [Your Hobbies/Interests] or
-        [Another Hobby/Interest]. Plus, I&apos;m always down for an adventure
-        and exploring new places whenever the opportunity arises.
-      </p>
-      <p className="mt-8">
-        This website is my way of sharing my [Your Field of Expertise] expertise
-        and connecting with like-minded folks. You can expect to find [What
-        Visitors Can Expect on Your Website]
-      </p>
-      <p className="mt-8">
-        Thanks for stopping by and I hope you dig getting to know me and my
-        work.
+        One of my biggest passions is traveling, getting to know new places,
+        customs and cultures from each location, when I&apos;m not working I
+        like to watch series, read books, do workout, play video games and
+        listen to music.
       </p>
     </section>
   )
